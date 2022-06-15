9fd3c11444097d149c403ebbae9934d73928ac6d
enerush <nyw077@gmail.com>
2022-06-15 14:08:18 +0300

    Added function to solve Recaptcha

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from anticaptchaofficial.recaptchav2proxyless import *
 
 def get_vin():
     vin = 'WBAVM1C51FV498337'
     return vin
 
-def solve_captcha():
-    pass
+def solve_captcha(url, sitekey):
+    solver = recaptchaV2Proxyless()
+    solver.set_verbose(1)
+    solver.set_key('7a0556a0ad5a2051060b0c45cd0d0740')
+    solver.set_website_url(url)
+    solver.set_website_key(sitekey)
+
+    g_response = solver.solve_and_return_solution()
+    if g_response != 0:
+        print('g-response: ' + g_response)
+    else:
+        print('task finished with error ' + solver.error_code)
+
 
 def get_pdf(vin):
-    driver = webdriver.Chrome(executable_path='/home/yn/Documents/chromedriver')
-    driver.get(url)
     vin_form = driver.find_element(By.NAME, "vin")
     vin_form.send_keys(vin)
-    capture_path = '/home/yn/Documents/screen.png'
     vin_form.send_keys(Keys.ENTER)
     driver.save_screenshot(capture_path)
 
@@ -22,11 +31,20 @@ def change_pdf():
     pass
 
 def main():
+    url = "https://bimmer.work"
     vin = get_vin()
-    print(vin)
-    get_pdf(vin)
 
-url = "https://bimmer.work"
+    driver = webdriver.Chrome(executable_path='/home/yn/Documents/chromedriver')
+    page = driver.get(url)
+
+    sitekey = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/form/div[3]/div').get_attribute('outerHTML')
+    sitekey_clean = sitekey.split('"><div style="width:')[0].split('data-sitekey="')[-1]
+    print('sitekey_clean', sitekey_clean)
+
+    solve_captcha(url, sitekey_clean)
+
+    vin_pdf = get_pdf(vin)
+
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,3 +1,29 @@
-vin = 'WBAVM1C51FV498337'
+from anticaptchaofficial.recaptchav2proxyless import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 url = "https://bimmer.work"
+driver = webdriver.Chrome(executable_path='/home/yn/Documents/chromedriver')
+page = driver.get(url)
+
+sitekey = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/form/div[3]/div').get_attribute('outerHTML')
+sitekey_clean = sitekey.split('"><div style="width:')[0].split('data-sitekey="')[-1]
+print('sitekey_clean', sitekey_clean)
+
+solver = recaptchaV2Proxyless()
+solver.set_verbose(1)
+solver.set_key('7a0556a0ad5a2051060b0c45cd0d0740')
+solver.set_website_url(url)
+solver.set_website_key(sitekey_clean)
+
+g_response = solver.solve_and_return_solution()
+if g_response != 0:
+    print('g-response: ' + g_response)
+else:
+    print('task finished with error ' + solver.error_code)
+
+driver.execute_script('document.getElementById("g-recaptcha-response").innerHTML = "{}";'.format(g_response))
+driver.execute_script("onSuccess('{}')".format(g_response))
+time.sleep(1)
 
