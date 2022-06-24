6b9c758ecac5f79d129932a9d017f1e0e5bee2e6
enerush <nyw077@gmail.com>
2022-06-24 18:24:36 +0300

    Added a check for previously processed PDFs Added description of functions.

--- save_pdf.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def save_pdf(link_pdf):
-    pdf_data = requests.get(link_pdf)
-    filename = vin
-    print(f'Saving {filename}')
-    with open('/home/yn/Downloads/' + filename, 'wb') as file:
-        file.write(pdf_data.content)
